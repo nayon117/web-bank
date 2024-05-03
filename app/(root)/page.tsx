b5e47a58d9498ac43e5d2d86244fabbb7f479be9
nayon117 <hasibul.nayon1@gmail.com>
2024-05-03 07:27:02 +0600

    create header box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,20 @@
+import HeaderBox from "@/components/HeaderBox";
 
 const Home = () => {
+    const loggedIn = {firstName:"Hasibul"}
     return(
-        <div>
-             <p> Welcome to Home page part </p>
-        </div>
+        <section className="home">
+             <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                    type="greeting"
+                    title="welcome"
+                    user={loggedIn?.firstName || "Guest"}
+                    subtext="Access and manage your account and transaction."
+                    />
+                </header>
+             </div>
+        </section>
     )
 }
 export default Home;
