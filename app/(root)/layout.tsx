9611cbe640b1a37f3036c63a49caa177ea79f6bd
nayon117 <hasibul.nayon1@gmail.com>
2024-05-03 09:30:36 +0600

    implement dynamic sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,21 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = {firstName:"Hasibul", lastName:"Hasan"}
+
   return (
-    <main>
-      sidebar
+    <main className="flex h-screen w-full font-inter">
+       <Sidebar user={loggedIn} />
+       <div className="flex size-full flex-col">
+        <div className="root-layout">
+
+        </div>
+       </div>
       {children}
     </main>
   );
