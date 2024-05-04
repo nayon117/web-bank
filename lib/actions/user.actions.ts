791ea1e1ffbee4c2fc9723e8e4317c85b283dc65
nayon117 <hasibul.nayon1@gmail.com>
2024-05-04 07:59:18 +0600

    succesfully implement signin functionality

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,8 +5,13 @@ import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { parseStringify } from "../utils";
 
-export const signIn = async () => {
+export const signIn = async ({email,password}:signInProps) => {
   try {
+
+    const { account } = await createAdminClient();
+    const response = await account.createEmailPasswordSession(email,password)
+    return parseStringify(response);
+    
   } catch (error) {
     console.error("Error", error);
   }
@@ -40,7 +45,8 @@ export const signUp = async (userData: SignUpParams) => {
 export async function getLoggedInUser() {
   try {
     const { account } = await createSessionClient();
-    return await account.get();
+     const user = await account.get();
+     return parseStringify(user);
   } catch (error) {
     return null;
   }
