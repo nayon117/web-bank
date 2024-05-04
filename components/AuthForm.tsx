36e7bfe1f1e68c7c5923ded5e040f75f57ffaa4e
nayon117 <hasibul.nayon1@gmail.com>
2024-05-04 06:59:17 +0600

    install appwrite

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -11,6 +11,7 @@ import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { signIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
@@ -43,16 +44,16 @@ const AuthForm = ({ type }: { type: string }) => {
     setIsLoading(true);
     try {
       if(type==='sign-up'){
-        // const newUser = await signUp(data)
+        const newUser = await signUp(data)
 
-        // setUser(newUser)
+        setUser(newUser)
       }
       if(type==='sign-in'){
-        // const response = await SignIn({
-        //   email:data.email,
-        //   password:data.password
-        // })
-        // if(response) router.push('/')
+        const response = await signIn({
+          email:data.email,
+          password:data.password
+        })
+        if(response) router.push('/')
       }
     } catch (error) {
       console.log(error);
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,31 @@
+"use server"
+
+import { createSessionClient } from "../appwrite"
+
+export const signIn = async() =>{
+    try {
+        
+    } catch (error) {
+        console.error('Error', error)
+    }
+}
+
+export const signUp = async(userData:SignUpParams) =>{
+    try {
+        
+    } catch (error) {
+        console.error('Error', error)
+    }
+}
+
+
+
+export async function getLoggedInUser() {
+    try {
+      const { account } = await createSessionClient();
+      return await account.get();
+    } catch (error) {
+      return null;
+    }
+  }
+  
