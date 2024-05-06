a95342681b63f734a85719854b275957c8e2fc05
nayon117 <hasibul.nayon1@gmail.com>
2024-05-06 09:49:34 +0600

    create my banks

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,9 +1,37 @@
+import BankCard from "@/components/BankCard";
+import HeaderBox from "@/components/HeaderBox";
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const MyBanks = () => {
-    return(
-        <div>
-             <p> Welcome to page part </p>
+const MyBanks =async () => {
+    const loggedIn =  await getLoggedInUser()
+    const accounts = await getAccounts({ 
+        userId: loggedIn.$id 
+      })
+  return (
+    <section className="flex">
+      <div className="my-banks">
+        <HeaderBox
+          title="My Banks Account"
+          subtext="Effortlessly Access and manage your account and transaction."
+        />
+        <div className="space-y-4">
+            <h2 className="header-2">
+                Your Cards
+            </h2>
+            <div className="flex flex-wrap gap-6">
+                {accounts && accounts.data.map((a:Account)=>(
+                    <BankCard
+                    key={a.id}
+                    account={a}
+                    userName={loggedIn?.firstName}
+                    />
+                
+                ))}
+            </div>
         </div>
-    )
-}
+      </div>
+    </section>
+  );
+};
 export default MyBanks;
--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,15 +1,17 @@
 import { formatAmount } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import Copy from "./Copy";
 
 const BankCard = ({
   account,
   userName,
   showBalance = true,
 }: CreditCardProps) => {
+  // console.log(account);
   return (
     <div className="flex flex-col">
-      <Link href="/" className="bank-card">
+      <Link href={`/transaction-history/?id=${account.appwriteItemId}`} className="bank-card" >
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">
@@ -29,7 +31,9 @@ const BankCard = ({
                 </h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-            ●●●● ●●●● ●●●●  <span className="text-16">1234</span>
+            ●●●● ●●●● ●●●●  <span className="text-16">
+              {account?.mask}
+            </span>
             </p>
           </article>
         </div>
@@ -57,6 +61,7 @@ const BankCard = ({
         className="absolute top-0 left-0"
         />
       </Link>
+      {showBalance && <Copy title={account?.sharaebleId} />}
     </div>
   );
 };
