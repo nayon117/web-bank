c816188f9412a40b7ddfe15a33779bb278edf310
nayon117 <hasibul.nayon1@gmail.com>
2024-05-06 07:35:23 +0600

    style the table

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -7,10 +7,14 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { formatAmount, formatDateTime, getTransactionStatus, removeSpecialCharacters } from "@/lib/utils";
+import {
+  formatAmount,
+  formatDateTime,
+  getTransactionStatus,
+  removeSpecialCharacters,
+} from "@/lib/utils";
 
-const TransactionsTable = ({transactions}: TransactionTableProps) => {
-  console.log(transactions)
+const TransactionsTable = ({ transactions }: TransactionTableProps) => {
   return (
     <Table>
       <TableHeader className="bg-[#f9fafb]">
@@ -24,40 +28,46 @@ const TransactionsTable = ({transactions}: TransactionTableProps) => {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {
-          transactions.map((t:Transaction)=>{
-            const status = getTransactionStatus(new Date(t.date))
-            const amount = formatAmount(t.amount)
+        {transactions.map((t: Transaction) => {
+          const status = getTransactionStatus(new Date(t.date));
+          const amount = formatAmount(t.amount);
 
-            const isDebit = t.type === "debit"
-            const isCredit = t.type === "credit"
+          const isDebit = t.type === "debit";
+          const isCredit = t.type === "credit";
 
-            return(
-              <TableRow key={t.id}>
-                <TableCell>
-                  <div>
-                    <h1> {removeSpecialCharacters(t.name)}</h1>
-                  </div>
-                </TableCell>
-                <TableCell>
-                  {isDebit ? `- ${amount}` : isCredit ? amount:amount}
-                </TableCell>
-                <TableCell>
-                  {status}
-                </TableCell>
-                <TableCell>
-                  {formatDateTime(new Date(t.date)).dateTime}
-                </TableCell>
-                <TableCell>
-                  {t.paymentChannel}
-                </TableCell>
-                <TableCell>
-                  {t.category}
-                </TableCell>
-              </TableRow>
-            )
-          })
-        }
+          return (
+            <TableRow
+              key={t.id}
+              className={`${
+                isDebit || amount[0] === "-" ? "bg-[#FFFBFA]" : "bg-[#F6FEF9]"
+              } !over:bg-none !border-b-DEFAULT `}
+            >
+              <TableCell className="max-w-[250px] pl-2 pr-10">
+                <div className="flex items-center gap-3">
+                  <h1 className="text-14 truncate font-semibold text-[#344054]">
+                    {" "}
+                    {removeSpecialCharacters(t.name)}
+                  </h1>
+                </div>
+              </TableCell>
+              <TableCell
+                className={`pl-2 pr-10 font-semibold ${
+                  isDebit || amount[0] === "-"
+                    ? "text-[#f04438]"
+                    : "text-[#039855]"
+                } `}
+              >
+                {isDebit ? `- ${amount}` : isCredit ? amount : amount}
+              </TableCell>
+              <TableCell className="pl-2 pr-10">{status}</TableCell>
+              <TableCell className="min-w-32 pl-2 pr-10">
+                {formatDateTime(new Date(t.date)).dateTime}
+              </TableCell>
+              <TableCell className="pl-2 pr-10 capitalize min-w-24">{t.paymentChannel}</TableCell>
+              <TableCell className="pl-2 pr-10 max-md:hidden">{t.category}</TableCell>
+            </TableRow>
+          );
+        })}
       </TableBody>
     </Table>
   );
